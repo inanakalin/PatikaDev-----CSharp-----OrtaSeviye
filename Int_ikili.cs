0673b54f056a73e3ab338b521bb1cb98769954d5
İnan Akalın <inan.akalin@gmail.com>
2023-03-27 15:05:27 +0300

    Int ikili

---
+++ Int_ikili.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtaSeviye
+{
+    internal class Int_ikili
+    {
+        public Int_ikili()
+        {
+            Console.WriteLine("Aralarına virgün bırakarak n adet sayı giriniz:     ");
+          
+            var input = Console.ReadLine();
+            var strings = input.Split(',');
+            String str = String.Empty;
+            for (int i = 0; i < strings.Length; i += 2)
+            {
+
+                if (strings[i] == strings[i + 1])
+                {
+                    str += Math.Pow((Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1])), 2) + " ";
+                }
+                else
+                {
+                    str += Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1]) + " ";
+                }
+            }
+            Console.WriteLine(str);
+
+        }
+    }
+}
