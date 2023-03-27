dc78ed0c026aeb8cfe6e3c56eaec8156da24a290
İnan Akalın <inan.akalin@gmail.com>
2023-03-27 17:22:02 +0300

    karakter değiştirme

---
+++ Karakter Değiştirme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrtaSeviye
+{
+    internal class Karakter_Değiştirme
+    {
+        public Karakter_Değiştirme()
+        {
+            Console.Write("Bir metin girin: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string[] newWords = new string[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= 1)
+                {
+                    newWords[i] = words[i];
+                }
+                else
+                {
+                    char firstChar = words[i][0];
+                    char lastChar = words[i][words[i].Length - 1];
+
+                    string newWord = lastChar + words[i].Substring(1, words[i].Length - 2) + firstChar;
+                    newWords[i] = newWord;
+                }
+            }
+        }
+    }
+}
